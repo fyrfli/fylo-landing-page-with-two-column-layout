c46b283d8bd34f29a5c17ed2be40b96bab3c1cb0
Camille Frantz <camille.frantz@me.com>
2022-04-23 20:34:48 -0500

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    fontFamily: {
+      'body': ['"Open Sans"'],
+      'nav': ['Raleway'],
+    },
+    extend: {
+      colors: {
+        'dark-blue': '#070439',
+        'desaturate-blue': '#585989',
+      }
+    },
+  },
+  plugins: [],
+}
